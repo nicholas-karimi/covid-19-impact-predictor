29ea82223f981ee1183193d546def65483143354
nicholas-karimi <kariminic@gmail.com>
2020-04-13 07:19:26 +0300

    implemented challenge 2

--- src/estimator.py
+++ src/estimator.py
@@ -4,34 +4,49 @@ def infectionsByTime(periodType, timeToElapse):
       if periodType == 'days':
             days = timeToElapse
             pair = (days / 3)
-            return (math.ceil(2 ** pair))
+            return (int(2 ** pair))
       elif periodType == 'weeks':
             days = timeToElapse * 7
             pair = (days / 3)
-            return (math.ceil(2** pair))
+            return (int(2** pair))
       elif periodType == 'months':
             days = timeToElapse * 30
             pair = (days / 3)
-            return (math.ceil(2 **pair))
+            return (int(2 **pair))
 
 
+
+def bedCapacity(totalHospitalBeds, severeCasesByRequestedTime):
+      occupiedBeds = int(totalHospitalBeds * 0.65)
+      availableBeds = totalHospitalBeds - occupiedBeds
+      availableForSevereCases = availableBeds - severeCasesByRequestedTime
+
+      return int(availableForSevereCases)
 
 def impact(data, impactType):
       reportedCases = data['reportedCases']
       if impactType == 'normal':
-            currentlyInfected = math.ceil(reportedCases * 10)
+            currentlyInfected = int(reportedCases * 10)
       elif impactType == 'severe':
-            currentlyInfected = math.ceil(reportedCases * 50)
+            currentlyInfected = int(reportedCases * 50)
       
       timeToElapse = data['timeToElapse']
       periodType = data['periodType']
 
       time = infectionsByTime(periodType, timeToElapse)
 
-      infectionsByRequestedTime = math.ceil(currentlyInfected * time)
+      infectionsByRequestedTime = int(currentlyInfected * time)
+
+      severeCasesByRequestedTime = int(infectionsByRequestedTime * 0.15)
+
+      totalBeds = data['totalHospitalBeds']
+
+      hospitalBedsByRequestedTime = bedCapacity(totalBeds, severeCasesByRequestedTime)
 
       return dict(currentlyInfected = currentlyInfected,
-                        infectionsByRequestedTime = infectionsByRequestedTime)
+                        infectionsByRequestedTime = infectionsByRequestedTime,
+                        severeCasesByRequestedTime = severeCasesByRequestedTime,
+                        hospitalBedsByRequestedTime = hospitalBedsByRequestedTime)
 
 def estimator(data):
       data = {
