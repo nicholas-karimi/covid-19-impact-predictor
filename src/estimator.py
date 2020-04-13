109db10c13ed52e634ec029e9bdfdfbd97246496
nicholas-karimi <kariminic@gmail.com>
2020-04-13 07:09:14 +0300

    change math computation

--- src/estimator.py
+++ src/estimator.py
@@ -4,31 +4,31 @@ def infectionsByTime(periodType, timeToElapse):
       if periodType == 'days':
             days = timeToElapse
             pair = (days / 3)
-            return (int(2 ** pair))
+            return (math.ceil(2 ** pair))
       elif periodType == 'weeks':
             days = timeToElapse * 7
             pair = (days / 3)
-            return (int(2** pair))
+            return (math.ceil(2** pair))
       elif periodType == 'months':
             days = timeToElapse * 30
             pair = (days / 3)
-            return (int(2 **pair))
+            return (math.ceil(2 **pair))
 
 
 
 def impact(data, impactType):
       reportedCases = data['reportedCases']
       if impactType == 'normal':
-            currentlyInfected = int(reportedCases * 10)
+            currentlyInfected = math.ceil(reportedCases * 10)
       elif impactType == 'severe':
-            currentlyInfected = int(reportedCases * 50)
+            currentlyInfected = math.ceil(reportedCases * 50)
       
       timeToElapse = data['timeToElapse']
       periodType = data['periodType']
 
       time = infectionsByTime(periodType, timeToElapse)
 
-      infectionsByRequestedTime = int(currentlyInfected * time)
+      infectionsByRequestedTime = math.ceil(currentlyInfected * time)
 
       return dict(currentlyInfected = currentlyInfected,
                         infectionsByRequestedTime = infectionsByRequestedTime)
