01bc1f451dcd326fe378253b35bf962b0c0c2b91
nicholas-karimi <kariminic@gmail.com>
2020-04-13 06:58:30 +0300

    fixed assertion error

--- src/estimator.py
+++ src/estimator.py
@@ -4,31 +4,31 @@ def infectionsByTime(periodType, timeToElapse):
       if periodType == 'days':
             days = timeToElapse
             pair = (days / 3)
-            return (2 ** pair)
+            return (math.pow(pair, 2))
       elif periodType == 'weeks':
             days = timeToElapse * 7
             pair = (days / 3)
-            return (2** pair)
+            return (math.pow(pair, 2))
       elif periodType == 'months':
             days = timeToElapse * 30
             pair = (days / 3)
-            return (2 **pair)
+            return (math.pow(pair,2))
 
 
 
 def impact(data, impactType):
       reportedCases = data['reportedCases']
       if impactType == 'normal':
-            currentlyInfected = int(reportedCases * 10)
+            currentlyInfected = math.floor(reportedCases * 10)
       elif impactType == 'severe':
-            currentlyInfected = int(reportedCases * 50)
+            currentlyInfected = math.floor(reportedCases * 50)
       
       timeToElapse = data['timeToElapse']
       periodType = data['periodType']
 
       time = infectionsByTime(periodType, timeToElapse)
 
-      infectionsByRequestedTime = int(currentlyInfected * time)
+      infectionsByRequestedTime = math.floor(currentlyInfected * time)
 
       return dict(currentlyInfected = currentlyInfected,
                         infectionsByRequestedTime = infectionsByRequestedTime)
@@ -41,7 +41,6 @@ def estimator(data):
             'severeImpact': impact(data, 'severe'),
       }
       return data
-
 
 
 
